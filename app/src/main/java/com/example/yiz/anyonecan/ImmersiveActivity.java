93285dcdc500d710d7a70ef57d268df578ff04e4
yochaiz <yochaizur@gmail.com>
2015-06-13 00:22:02 +0300

    Immersive mode volume buttons bug fix

--- app/src/main/java/com/example/yiz/anyonecan/ImmersiveActivity.java
+++ app/src/main/java/com/example/yiz/anyonecan/ImmersiveActivity.java
@@ -1,13 +1,51 @@
 package com.example.yiz.anyonecan;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class ImmersiveActivity extends Activity {
 
+    private final static int uiType = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    private Runnable decor_view_settings = new Runnable() {
+        public void run() {
+            getWindow().getDecorView().setSystemUiVisibility(uiType);
+        }
+    };
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            mHandler.postDelayed(decor_view_settings, 500);
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            mHandler.post(decor_view_settings);
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        mHandler.post(decor_view_settings);
     }
 }
